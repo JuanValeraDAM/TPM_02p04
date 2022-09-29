d560cf517f64cf66327432af71c3f70bcffaced0
JuanValeraDAM <3690820@alu.murciaeduca.es>
2022-09-29 02:37:53 +0200

    Layout Calculadora

--- app/src/main/java/com/dam/t02p04/MainActivity.java
+++ app/src/main/java/com/dam/t02p04/MainActivity.java
@@ -3,12 +3,17 @@ package com.dam.t02p04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.dam.t02p04.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
